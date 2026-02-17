647c8201eb7ac2fa2091a22d365a725d5429cbe3
Robert Zydek <zrobert@cisco.com>
2026-02-17 19:45:48 +0100

    chore: replace css with scss

--- apps/shell/src/app/app.ts
+++ apps/shell/src/app/app.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { NxWelcome } from './nx-welcome';
 
 @Component({
-    imports: [NxWelcome, RouterModule],
+    imports: [RouterModule],
     selector: 'app-root',
     templateUrl: './app.html',
-    styleUrl: './app.css',
+    styleUrl: './app.scss',
 })
 export class App {
     protected title = 'shell';
